1276091b62deb6799bf5708e67ee12fd8477d6d6
atramUCSD <atram@ucsd.edu>
2021-02-25 10:59:25 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,11 @@
-function initMap() {
-	// add your code here
+function initMap(){
+	L.mapquest.key = 'Fj4dlAkBF5G197biOGOfJXEGQOCCRVXE';
+	
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {center: [32.88, -117.236],
+	layers: L.mapquest.tileLayer('map'), zoom: 12,
+	zoomControl: false});
+	
+	// Add a market to the map
+	L.marker([32.88, -117.236]).addTo(map);
 }
